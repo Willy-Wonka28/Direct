660b39baa70ecbd6bfa95a96fe60326a0dc4ea5c
Oyetibo Olaoluwa <techpro@gmail.com>
2025-03-22 19:35:56 +0100

    listen changes

--- frontend/direct_frontend/src/Websockets/listenUpdates.tsx
+++ frontend/direct_frontend/src/Websockets/listenUpdates.tsx
@@ -2,9 +2,11 @@ import { io } from "socket.io-client";
 
 const socket = io("https://direct-production.up.railway.app/transactions", {});
 
-export const listenForTransactionUpdates = (updateTransactionStatus: (data: any) => void) => {
+export const listenForTransactionUpdates = (
+  updateTransactionStatus: (data: any) => void
+) => {
   socket.on("transaction_update", (data) => {
-    console.log("Received transaction update:", data);
-    updateTransactionStatus(data); 
+    console.log("📡 Received transaction update:", data); // 👈 Check if this logs in the console
+    updateTransactionStatus(data);
   });
 };
